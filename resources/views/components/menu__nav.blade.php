5c9f0a6b61a9fb7ca02adbaea38bd8a38fd30d9e
abraham ghaemi <=>
2019-05-11 03:38:01 +0430

    update

--- resources/views/components/menu__nav.blade.php
+++ resources/views/components/menu__nav.blade.php
@@ -1,12 +1,14 @@
 <div class="menu">
   <nav class="menu__nav">
     <div class="container row" style="top: 10px;position: absolute;left: 15px;">
-  <div class="col-12">
+  <div class="col-12" style="top: 20px">
     <div class="list-group" id="list-tab" role="tablist">
       <a class="list-group-item" data-toggle="modal" data-target="#contact" id="list-home-list" href="#list-home">در خواست نوبت</a>
       <a class="list-group-item" data-toggle="modal" data-target="#oppoinment" id="list-profile-list" href="#list-profile">درخواست مشاوره</a>
       <a class="list-group-item" data-toggle="modal" data-target="#case"   href="#list-messages">پیشنهادات و انتقادات</a>
       <a class="list-group-item"  target="_blank" href="https://www.google.com/maps/?q=35.763770, 51.288649">آدرس مطب روی نقشه</a>
+      <a class="list-group-item"  target="_blank" href="tel:0218770000">تماس با ما</a>
+      <a class="list-group-item"  target="_blank" href="tel:0218770000">مشاوره مستقیم</a>
       <a class="list-group-item"  target="_blank" href="http://api.whatsapp.com/send?phone09128981521=&text=با سلام، اینجانب در خواست مشاوره دارم">واتسپ</a>
       <a class="list-group-item" id="list-settings-list"  target="_blank" href="https://telegram.me/ModernClinique">تلکرام</a>
     </div>
@@ -15,7 +17,7 @@
   </nav>
   <div class="hamburger">
     <button class="r-button hamburger__button menu__toggle">
-      <span class="hamburger__label">Open menu</span>
+      <i class="fas fa-phone-square"></i>
     </button>
   </div>  
 </div>
