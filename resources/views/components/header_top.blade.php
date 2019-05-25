ebc000beb71ba599f568a50cad43ca98ff6362a4
ibrahem ghaemi <irvato@icloud.com>
2019-05-25 13:24:00 +0430

    update

--- resources/views/components/header_top.blade.php
+++ resources/views/components/header_top.blade.php
@@ -3,18 +3,19 @@
                     <div class="row ">
                         <div class="col-lg-6 col-md-8 col-12">
                             <div class="ht__header__top__left">
-                                <p class="social_icon">
-                                    مشاوره آنلاین
-                                    <a href="http://api.whatsapp.com/send?phone09128981521=&text=با سلام، اینجانب در خواست مشاوره دارم" target="_blank"><i class="fa fa-whatsapp"></i></a>
-                                    <a href="https://telegram.me/ModernClinique" target="_blank"><i class="fa fa-telegram"></i></a>
-                                </p>
+                                <p>کلینیک تخصصی پوست و مو و زیبایی - مدرن </p>
+                                <p><span><i class="zmdi zmdi-phone"></i></span> شماره تماس: ۰۲۱-۸۷۷۰۰۰۰۰</p>
                                 
                             </div>
                         </div>
                         <div class="col-lg-6 col-md-4 col-12">
                             <div class="ht__header__top__right">
-                                <p>کلینیک تخصصی پوست و مو و زیبایی - مدرن <span><i class="zmdi zmdi-phone"></i></span> شماره تماس: ۰۲۱-۸۷۷۰۰۰۰۰</p>
                                 <p>همه روزه ۸:۰۰ صبح الی ۹:۰۰ شب <span><i class="zmdi zmdi-alarm-check"></i></span></p>
+                                <p class="social_icon">
+                                    مشاوره آنلاین
+                                    <a href="http://api.whatsapp.com/send?phone09128981521=&text=با سلام، اینجانب در خواست مشاوره دارم" target="_blank"><i class="fa fa-whatsapp"></i></a>
+                                    <a href="https://telegram.me/ModernClinique" target="_blank"><i class="fa fa-telegram"></i></a>
+                                </p>
                             </div>
                         </div>
                     </div>
