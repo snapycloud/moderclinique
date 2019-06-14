b25c59d5f0cb7dfe8ad9c0803d72f018d45103a7
abraham ghaemi <=>
2019-06-14 23:46:40 +0430

    update header css

--- resources/views/components/header_top.blade.php
+++ resources/views/components/header_top.blade.php
@@ -1,17 +1,17 @@
             <div class="htc__header__top">
                 <div class="container">
                     <div class="row ">
-                        <div class="col-lg-6 col-md-8 col-12">
+                        <div class="col-lg-6 col-md-8 col-12 res-col">
                             <div class="ht__header__top__left">
                                 <p class="ht__header__top__p__left social_icon">
                                     مشاوره آنلاین
                                     <a href="http://api.whatsapp.com/send?phone09128981521=&text=با سلام، اینجانب در خواست مشاوره دارم" target="_blank"><i class="fa fa-whatsapp"></i></a>
                                     <a href="https://telegram.me/ModernClinique" target="_blank"><i class="fa fa-telegram"></i></a>
                                 </p>
-                                <p><span><i class="zmdi zmdi-phone"></i></span> شماره تماس: ۸۷۷۰۰۰۰۰-۰۲۱</p>                                
+                                <p></span> شماره تماس: <a href="tel:02188770000">۸۷۷۰۰۰۰۰-۰۲۱</a><span><i class="zmdi zmdi-phone"></i></p>                                
                             </div>
                         </div>
-                        <div class="col-lg-6 col-md-4 col-12">
+                        <div class="col-lg-6 col-md-4 col-12 res-col-top">
                             <div class="ht__header__top__right">
                                 <p class=""><storng style="font-size: 16px;">کلینیک تخصصی پوست و مو و زیبایی - مدرن </storng><br> همه روزه ۸:۰۰ صبح الی ۹:۰۰ شب <span><i class="zmdi zmdi-alarm-check"></i></span></p>
                             </div>
